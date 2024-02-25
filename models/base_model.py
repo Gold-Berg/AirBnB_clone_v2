24bb9a6ca7c8de1d6c296187fbc601a468f353d5
Gold-Berg <igbinobaeseosa0@gmail.com>
2024-02-24 17:59:25 -0800

    Task 6.6 update

--- models/base_model.py
+++ models/base_model.py
@@ -18,23 +18,20 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if kwargs:
-            if "id" not in kwargs.keys():
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != "__class__":
+                    setattr(self, key, value)
+            if "id" not in kwargs:
                 self.id = str(uuid.uuid4())
-            if "updated_at" not in kwargs.keys():
-                self.updated_at = datetime.now()
-            else:
-                kwargs['updated_at'] = datetime.strptime(
-                        kwargs['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
-            if "created_at" not in kwargs.keys():
+            if "created_at" not in kwargs:
                 self.created_at = datetime.now()
-            else:
-                kwargs['created_at'] = datetime.strptime(
-                        kwargs['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
-            self.__dict__.update(kwargs)
+            if "updated_at" not in kwargs:
+                self.updated_at = datetime.now()
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+            self.created_at = self.updated_at = datetime.now()
 
     def __str__(self):
         """Returns a string representation of the instance"""
