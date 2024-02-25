3e6d94f6199f38f4a3ac2db17f89f47444974210
Gold-Berg <igbinobaeseosa0@gmail.com>
2024-02-24 17:14:45 -0800

    Task 5 update

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -59,6 +59,9 @@ class FileStorage:
     def delete(self, obj=None):
         """ delete an existing element
         """
-        if obj:
-            key = "{}.{}".format(type(obj).__name__, obj.id)
+        if obj is None:
+            return
+        key = obj.__class__.__name__ + '.' + obj.id
+        if key in self.__objects:
             del self.__objects[key]
+            self.save()
