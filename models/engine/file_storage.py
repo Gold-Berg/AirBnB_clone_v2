ce8e5e0ac91470ccf17b2c5c1696b3acb1c62582
Ebi-Fredrick Emmanuel <ebifredrick07@gmail.com>
2024-02-24 19:02:06 -0800

    Need to revert

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,16 +8,9 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self, cls=None):
-        """Returns a dictionary of models in storage"""
-        if cls is None:
-            return FileStorage.__objects
-        _dict = {}
-        for key, val in FileStorage.__objects.items():
-            if isinstance(val, cls):
-                _dict[key] = val
-        return _dict
-
+    def all(self):
+        """Returns a dictionary of models currently in storage"""
+        return FileStorage.__objects
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
